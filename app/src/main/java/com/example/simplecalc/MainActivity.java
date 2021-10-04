17dcf181f530de94273a91c092a0b14ac269c9a5
Mr-Ramani <vasoyakhimji@gmail.com>
2021-10-04 06:35:17 +0530

    improved ui and better button

--- app/src/main/java/com/example/simplecalc/MainActivity.java
+++ app/src/main/java/com/example/simplecalc/MainActivity.java
@@ -1,62 +1,214 @@
-package com.example.simplecalc;
+package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity{
 
-    Button b;
-    EditText E1,E2;
+    Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18,b19;
+    TextView tx;
+    Boolean add,sub,div,mul,mod;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    b=findViewById(R.id.add);
-    b.setOnClickListener(this);
-    E1=findViewById(R.id.e1);
-    E2=findViewById(R.id.e2);
-    }
-    @Override
-    public void onClick(View view)
-    {
-        String a= E1.getText().toString();
-        String b=E2.getText().toString();
-        int n1=Integer.parseInt(a);
-        int n2=Integer.parseInt(b);
-        int sum =n1+n2;
-        Toast.makeText(this,"addition:"+sum,Toast.LENGTH_LONG).show();
 
-    }
+        tx=findViewById(R.id.screen);
+        b1=findViewById(R.id.btn1);
+        b2=findViewById(R.id.btn2);
+        b3=findViewById(R.id.btn3);
+        b4=findViewById(R.id.btn4);
+        b5=findViewById(R.id.btn5);
+        b6=findViewById(R.id.btn6);
+        b7=findViewById(R.id.btn7);
+        b8=findViewById(R.id.btn8);
+        b9=findViewById(R.id.btn9);
+        b10=findViewById(R.id.btn10);
+        b11=findViewById(R.id.btn11);
+        b12=findViewById(R.id.btn12);
+        b13=findViewById(R.id.btn13);
+        b14=findViewById(R.id.btn14);
+        b15=findViewById(R.id.btn15);
+        b16=findViewById(R.id.btn16);
+        b17=findViewById(R.id.btn17);
+        b18=findViewById(R.id.btn18);
+        b19=findViewById(R.id.btn19);
 
-    public void division(View view) {
-    String a= E1.getText().toString();
-    String b=E2.getText().toString();
-    double n1=Double.parseDouble(a);
-    double n2=Double.parseDouble(b);
-    double ans=n1/n2;
-    Toast.makeText(this,"division:"+ans,Toast.LENGTH_LONG).show();
-}
+        b2.setText("<");
+        add=sub=div=mul=mod=false;
 
-    public void multiplication(View view) {
-        String a= E1.getText().toString();
-        String b=E2.getText().toString();
-        int n1=Integer.parseInt(a);
-        int n2=Integer.parseInt(b);
-        int mul =n1*n2;
-        Toast.makeText(this,"multiplication:"+mul,Toast.LENGTH_LONG).show();
-    }
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(" ");
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(" ");
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(tx.getText()+"%");
+                mod=true;
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(tx.getText()+"/");
+                div=true;
+            }
+        });
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(tx.getText()+"7");
+            }
+        });
 
-    public void subtraction(View view) {
-        String a= E1.getText().toString();
-        String b=E2.getText().toString();
-        int n1=Integer.parseInt(a);
-        int n2=Integer.parseInt(b);
-        int sub =n1-n2;
-        Toast.makeText(this,"subtraction:"+sub,Toast.LENGTH_LONG).show();
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(tx.getText()+"8");
+            }
+        });
+
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(tx.getText()+"9");
+            }
+        });
+
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(tx.getText()+"X");
+                mul=true;
+            }
+        });
+        b9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(tx.getText()+"4");
+            }
+        });
+        b10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(tx.getText()+"5");
+            }
+        });
+        b11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(tx.getText()+"6");
+            }
+        });
+        b12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(tx.getText()+"+");
+                add=true;
+            }
+        });
+        b13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(tx.getText()+"1");
+            }
+        });
+        b14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(tx.getText()+"2");
+            }
+        });
+        b15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(tx.getText()+"3");
+            }
+        });
+        b16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(tx.getText()+"-");
+                sub=true;
+            }
+        });
+
+        b17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(tx.getText()+"0");
+            }
+        });
+        b18.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText(tx.getText()+".");
+            }
+        });
+        b19.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(add==true){
+                    String s = tx.getText().toString();
+                    int index = s.indexOf("+");
+                     String val1 = s.substring(0,index);
+                     String val2 = s.substring(index+1);
+
+                     float ans=Float.parseFloat(val1)+Float.parseFloat(val2);
+                     tx.setText(""+ans);
+                }
+                if(sub==true){
+                    String s = tx.getText().toString();
+                    int index = s.indexOf("-");
+                    String val1 = s.substring(0,index);
+                    String val2 = s.substring(index+1);
+
+                    float ans=Float.parseFloat(val1)-Float.parseFloat(val2);
+                    tx.setText(""+ans);
+                }
+                if(div==true){
+                    String s = tx.getText().toString();
+                    int index = s.indexOf("/");
+                    String val1 = s.substring(0,index);
+                    String val2 = s.substring(index+1);
+
+                    float ans=Float.parseFloat(val1)/Float.parseFloat(val2);
+                    tx.setText(""+ans);
+                }
+                if(mod==true){
+                    String s = tx.getText().toString();
+                    int index = s.indexOf("%");
+                    String val1 = s.substring(0,index);
+                    String val2 = s.substring(index+1);
+
+                    float ans=Float.parseFloat(val1)%Float.parseFloat(val2);
+                    tx.setText(""+ans);
+                }
+                if(mul==true){
+                    String s = tx.getText().toString();
+                    int index = s.indexOf("X");
+                    String val1 = s.substring(0,index);
+                    String val2 = s.substring(index+1);
+
+                    float ans=Float.parseFloat(val1)*Float.parseFloat(val2);
+                    tx.setText(""+ans);
+                }
+            }
+        });
+
     }
 }
